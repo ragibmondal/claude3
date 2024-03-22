0ccbeb7a7eff515734453d2033735e6dc09a9085
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2024-03-22 16:30:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,106 @@
 import streamlit as st
 import anthropic
-from claude import ClaudeLlm  # claude.py から ClaudeLlm クラスをインポート
+from claude import ClaudeLlm  # Import the ClaudeLlm class from claude.py
 import os
 import base64
+import os
 
-# Receive input from sidebar instead of getting API key from environment variable
-api_key = st.sidebar.text_input("Enter your API key", type="password", help="Enter your API key here.")
+# Get API key from the environment variable
+api_key = os.environ.get("ANTHROPIC_API_KEY")
 
-# If the API key is not entered, display a warning and abort the process
+# If the API key is not provided, display an error message and exit
 if not api_key:
-     st.error("API key required. Please enter it from the sidebar.")
-else:
-     client = anthropic.Anthropic(api_key=api_key)
-    
+   st.error("API key is not set in the environment variable. Please set it and try again.")
+   st.stop()
+
+client = anthropic.Anthropic(api_key=api_key)
+
 # Mode selection
 mode = st.sidebar.radio("Please choose a mode", ('Text', 'Vision'))
 
 if mode == 'Text':
-     # Streamlit app title
-     st.title("Claude3-Streamlit")
-     # receive user input
-     user_input = st.text_area("Please enter text", placeholder="What is the second highest mountain in Japan? Please just tell me the name", height=200)
-     # Processing when the send button is pressed
-     if st.button("Send"):
-         # Create an instance of ClaudeLlm class
-         claude = ClaudeLlm(client, user_input)
-        
-         col1, col2 = st.columns(2) # Split the screen into two columns
-         with col1:
-             st.write("Opus response:")
-             st.write_stream(claude.generate_responses("claude-3-opus-20240229"))
-         with col2:
-             st.write("Sonnet response:")
-             st.write_stream(claude.generate_responses("claude-3-sonnet-20240229"))
-         # Display cost calculation results
-         st.table(claude.cost_df)
-         _, jpy_rate = claude.convert_usd_to_jpy(1) # dummy call to get the exchange rate of 1USD
-         st.write(jpy_rate)
+   # Streamlit app title
+   st.title("Claude3-Streamlit")
+
+   # Receive user input
+   user_input = st.text_area("Please enter text", placeholder="What is the second highest mountain in Japan? Please just tell me the name", height=200)
+
+   # Processing when the send button is pressed
+   if st.button("Send"):
+       # Create an instance of ClaudeLlm class
+       claude = ClaudeLlm(client, user_input)
+       col1, col2 = st.columns(2)  # Split the screen into two columns
+
+       with col1:
+           st.write("Opus response:")
+           st.write_stream(claude.generate_responses("claude-3-opus-20240229"))
+
+       with col2:
+           st.write("Sonnet response:")
+           st.write_stream(claude.generate_responses("claude-3-sonnet-20240229"))
+
+       # Display cost calculation results
+       st.table(claude.cost_df)
+
+       _, jpy_rate = claude.convert_usd_to_jpy(1)  # dummy call to get the exchange rate of 1USD
+       st.write(jpy_rate)
 
 elif mode == 'Vision':
-     st.title("Claude3-Streamlit-vision")
-     uploaded_file = st.file_uploader("Please select an image file", type=["jpg", "jpeg", "png", "gif"])
+   st.title("Claude3-Streamlit-vision")
 
-     if uploaded_file is not None:
-         st.image(uploaded_file, caption="Uploaded image", use_column_width=True)
-         # get file extension
-         prompt = st.text_area("Please enter text", placeholder="Describe the image.", height=200)
-         file_extension = os.path.splitext(uploaded_file.name)[1].lower()
+   uploaded_file = st.file_uploader("Please select an image file", type=["jpg", "jpeg", "png", "gif"])
 
-         # set media type based on extension
-         if file_extension in [".jpg", ".jpeg"]:
-             image_media_type = "image/jpeg"
-         elif file_extension == ".png":
-             image_media_type = "image/png"
-         elif file_extension == ".gif":
-             image_media_type = "image/gif"
-         else:
-             st.error("Unsupported file format.")
-             st.stop()
+   if uploaded_file is not None:
+       st.image(uploaded_file, caption="Uploaded image", use_column_width=True)
 
-         image_data = base64.b64encode(uploaded_file.read()).decode("utf-8")
+       # Get file extension
+       prompt = st.text_area("Please enter text", placeholder="Describe the image.", height=200)
+       file_extension = os.path.splitext(uploaded_file.name)[1].lower()
 
-         # prompt settings
+       # Set media type based on extension
+       if file_extension in [".jpg", ".jpeg"]:
+           image_media_type = "image/jpeg"
+       elif file_extension == ".png":
+           image_media_type = "image/png"
+       elif file_extension == ".gif":
+           image_media_type = "image/gif"
+       else:
+           st.error("Unsupported file format.")
+           st.stop()
 
-         user_input = [
-             {
-                 "type": "image",
-                 "source": {
-                     "type": "base64",
-                     "media_type": image_media_type,
-                     "data": image_data,
-                 },
-             },
-             {
-                 "type": "text",
-                 "text": prompt
-             }
-         ]
-         if st.button("Send"):
-             # Create an instance of ClaudeLlm class
-             claude = ClaudeLlm(client, user_input)
-             col1, col2 = st.columns(2) # Split the screen into two columns
-             with col1:
-                 st.write("Opus response:")
-                 st.write_stream(claude.generate_responses("claude-3-opus-20240229"))
-             with col2:
-                 st.write("Sonnet response:")
-                 st.write_stream(claude.generate_responses("claude-3-sonnet-20240229"))
-             # Display cost calculation results
-             st.table(claude.cost_df)
-             _, jpy_rate = claude.convert_usd_to_jpy(1) # dummy call to get the exchange rate of 1USD
-             st.write(jpy_rate)
+       image_data = base64.b64encode(uploaded_file.read()).decode("utf-8")
+
+       # Prompt settings
+       user_input = [
+           {
+               "type": "image",
+               "source": {
+                   "type": "base64",
+                   "media_type": image_media_type,
+                   "data": image_data,
+               },
+           },
+           {
+               "type": "text",
+               "text": prompt
+           }
+       ]
+
+       if st.button("Send"):
+           # Create an instance of ClaudeLlm class
+           claude = ClaudeLlm(client, user_input)
+           col1, col2 = st.columns(2)  # Split the screen into two columns
+
+           with col1:
+               st.write("Opus response:")
+               st.write_stream(claude.generate_responses("claude-3-opus-20240229"))
+
+           with col2:
+               st.write("Sonnet response:")
+               st.write_stream(claude.generate_responses("claude-3-sonnet-20240229"))
+
+           # Display cost calculation results
+           st.table(claude.cost_df)
+
+           _, jpy_rate = claude.convert_usd_to_jpy(1)  # dummy call to get the exchange rate of 1USD
+           st.write(jpy_rate)
